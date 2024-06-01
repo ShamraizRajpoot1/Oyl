638e5a21cefeb7abc6f9ee52962acf2e4f020732
ShamraizRajpoot1 <sshamraiz4@gmail.com>
2024-06-01 22:04:33 +0500

    Update index.js

--- src/navigation/Auth/index.js
+++ src/navigation/Auth/index.js
@@ -1,7 +1,7 @@
 import React, {useContext, useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import SignIn from '../../screens/authFlow/Signin';
-import SignUp from '../../screens/authFlow/Signup';
+import SignIn from '../../screens/authFlow/signin';
+import SignUp from '../../screens/authFlow/signup';
 import Profile from '../../screens/authFlow/Profile';
 import PrivacyPolicy from '../../screens/appFlow/PrivacyPolicy';
 import TermsOfService from '../../screens/appFlow/TermsOfService';
